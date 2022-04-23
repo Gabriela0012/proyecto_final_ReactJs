384950040ec635d5c53b7e81e0739a676bd07950
gabriela Arenas <gabega10@gmail.com>
2022-04-23 17:48:58 -0300

    cambio navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import './App.css';
-import logo from './imagenes/logo.png';
+
 
 import NavBar from './components/NavBar/NavBar';
-import CartWidget from './components/CartWidget';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <div className="navegacion">
-        <img src={logo} className='img-logo'alt='logo' />
-        <NavBar /> 
-        <CartWidget />
-      </div>
+      <NavBar /> 
       <ItemListContainer greeting='SI NO ES UN BUEN DÍA ME LO INVENTO LOVELY'/>
           
       <header className="App-header">
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import './NavBar.css';
-
+import logo from './../../imagenes/logo.png';
+import CartWidget from './CartWidget';
 
 function NavBar() {
   return (
-    <div className='contenedor-navbar'>
-      <div>
-        <ul className='nav'>
-          <li>
-            <a href='#'>INICIO</a>
-          </li>
-          <li>
-            <a  href='#'>PRODUCTOS</a>
-          </li>
-          <li>
-            <a href='#'>INFORMACIÓN PARA COMPRAR</a>
-          </li>
-          <li>
-            <a href='#'>TABLA DE TALLES</a>
-          </li>
-          <li>
-            <a href='#'>CONTACTANOS</a>
-          </li>
-        </ul>
+    <div className='navegacion'>
+      <img src={logo} className='img-logo'alt='logo' />
+      <div className='contenedor-navbar'>
+        <div>
+          <ul className='nav'>
+            <li>
+              <a href='#'>INICIO</a>
+            </li>
+            <li>
+              <a  href='#'>PRODUCTOS</a>
+            </li>
+            <li>
+              <a href='#'>INFORMACIÓN PARA COMPRAR</a>
+            </li>
+            <li>
+              <a href='#'>TABLA DE TALLES</a>
+            </li>
+            <li>
+              <a href='#'>CONTACTANOS</a>
+            </li>
+          </ul>
 
-      </div>
-      
+        </div>
+
     </div>
+    
+    <CartWidget />
+  </div>
   );
 }
 
